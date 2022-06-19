480bc67199e4a120ecf2e5e325d3afe685a68ca2
Jason Louie <jason.jw.louie@gmail.com>
2022-06-19 14:53:20 -0700

    works now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { connect } from 'mongoose';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- pages/api/url.ts
+++ pages/api/url.ts
@@ -1,13 +1,45 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+//make url route
+import url_model from '../../models/url'
+import { connect } from 'mongoose'
+
+function getString(length = 5){
+  var k = ""
+  for (var i = 0; i<length; i++){
+      var m = Math.round(65+ Math.random()*57)
+      
+      while (91 <= m && m <= 96){
+          m = Math.round(65+ Math.random()*57)
+      }
+          
+      k += String.fromCharCode(m)
+  }
+  
+  return k;
+}
+
+async function db_connect() {
+  await connect('mongodb+srv://Chubkey:booman3000@images.v2rim.mongodb.net/?retryWrites=true&w=majority'); 
+}
+
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  url: String
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  await db_connect();
+
+  const url = new url_model({
+    url: getString(),
+    target: req.body.url
+  })
+
+  const test = await url.save();
+
+  res.status(200).json({url: req.headers.host + '/' + url.url})
 }
